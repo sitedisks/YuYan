ca44ac8ecf8b886c7e48ac925dec527148d9688c
Peter <peter@sitedisks.com.au>
2016-01-21 07:56:40 +1100

    update poco Survey table

---
+++ YuYan.API/YuYan.Data/Database/tbSurvey.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace YuYan.Data.Database
+{
+    [Table("Survey")]
+    public class tbSurvey
+    {
+        [Key]
+        [Column("Id")]
+        public int SurveyId { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+    }
+}
