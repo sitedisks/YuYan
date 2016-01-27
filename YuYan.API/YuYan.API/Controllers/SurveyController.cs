8f07cf17ae1c536ebc3da22e5b99717542c4c1af
Peter Wang <peter@sitedisks.com.au>
2016-01-27 16:11:18 +1100

    Restful API format (to be continue...)

--- YuYan.API/YuYan.API/Controllers/SurveyController.cs
+++ YuYan.API/YuYan.API/Controllers/SurveyController.cs
@@ -6,7 +6,7 @@ using YuYan.Interface.Service;
 
 namespace YuYan.API.Controllers
 {
-    [RoutePrefix("survey")]
+    [RoutePrefix("surveys")]
     public class SurveyController : ApiController
     {
         private readonly IYuYanService _yuyanSvc;
@@ -38,7 +38,7 @@ namespace YuYan.API.Controllers
             return Ok(dtoSurvey);
         }
 
-        [HttpPost]
+        [Route(""), HttpPost]
         public async Task<IHttpActionResult> CreateSurvey([FromBody] dtoSurvey survey) {
             dtoSurvey dtoSurvey = new dtoSurvey();
 
@@ -57,7 +57,7 @@ namespace YuYan.API.Controllers
             return Ok(dtoSurvey);
         }
 
-        [HttpPut]
+        [Route(""), HttpPut]
         public async Task<IHttpActionResult> UpdateSurvey([FromBody] dtoSurvey survey)
         {
             dtoSurvey dtoSurvey = new dtoSurvey();
@@ -77,7 +77,7 @@ namespace YuYan.API.Controllers
             return Ok(dtoSurvey);
         }
 
-        [HttpDelete]
+        [Route("{surveyid}"), HttpDelete]
         public async Task<IHttpActionResult> DeleteSurvey(int surveyId) {
             try {
                 await _yuyanSvc.DeleteSurvey(surveyId);
@@ -93,16 +93,77 @@ namespace YuYan.API.Controllers
             return Ok();
         }
 
-
-
+        /*
         [Route("user/{userid}"), HttpGet]
         public IHttpActionResult GetAllSurveysByUserId(Guid userId) {
 
             return Ok();
         }
+         */
         #endregion
 
         #region question
+        [Route("{surveyid}/questions"), HttpGet]
+        public async Task<IHttpActionResult> GetQuestionBySurveyId(int surveyId) {
+
+            return Ok();
+        }
+
+        [Route("{surveyid}/questions"), HttpPost]
+        public async Task<IHttpActionResult> CreateQuestion(int surveyId, [FromBody] dtoSurveyQuestion question)
+        {
+            dtoSurveyQuestion dtoQuestion = new dtoSurveyQuestion();
+            try {
+                question.SurveyId = surveyId;
+                dtoQuestion = await _yuyanSvc.CreateSurveyQuestion(question);
+            }
+            catch (ApplicationException aex)
+            {
+                return BadRequest(aex.Message);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+            return Ok(dtoQuestion);
+        }
+
+        [Route("{surveyid}/questions"), HttpPut]
+        public async Task<IHttpActionResult> UpdateQuestion(int surveyId, [FromBody] dtoSurveyQuestion question)
+        {
+            dtoSurveyQuestion dtoSurveyQuestion = new dtoSurveyQuestion();
+
+            try {
+                question.SurveyId = surveyId;
+                dtoSurveyQuestion = await _yuyanSvc.UpdateSurveyQuestion(question);
+            }
+            catch (ApplicationException aex)
+            {
+                return BadRequest(aex.Message);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
+            return Ok(dtoSurveyQuestion);
+        }
+
+        [Route("{surveyid}/questions/{questionId}"), HttpDelete]
+        public async Task<IHttpActionResult> DeleteQuestion(int questionId) {
+            try {
+                await _yuyanSvc.DeleteSurveyQuestion(questionId);
+            }
+            catch (ApplicationException aex)
+            {
+                return BadRequest(aex.Message);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+            return Ok();
+        }
         #endregion
 
         #region item
