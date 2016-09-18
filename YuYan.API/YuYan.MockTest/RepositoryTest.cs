4216d679e604fbaccac1897e26743b23857dbe5d
Peter Wang <peter@sitedisks.com.au>
2016-09-19 06:55:10 +1000

    xUnit and NSubstitute initail

--- YuYan.API/YuYan.MockTest/RepositoryTest.cs
+++ YuYan.API/YuYan.MockTest/RepositoryTest.cs
@@ -1,14 +1,51 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace YuYan.MockTest
+﻿namespace YuYan.MockTest
 {
-    [TestClass]
+    using System;
+    using Xunit;
+    using NSubstitute;
+
     public class RepositoryTest
     {
-        [TestMethod]
-        public void TestMethod1()
+
+        public interface ICalculator {
+            int Add(int a, int b);
+            string Mode { get; set; }
+            event EventHandler PoweringUp;
+        }
+
+        [Fact]
+        public void Prac_Test()
         {
+            var mockCalculator = Substitute.For<ICalculator>();
+            mockCalculator.Add(1, 2).Returns(3);
+
+            
+            var result = mockCalculator.Add(1,2);
+            mockCalculator.Received().Add(1, 2);
+            mockCalculator.DidNotReceive().Add(3, 2);
+            Assert.Equal<int>(3, result);
+        }
+
+        [Fact]
+        public void Prac2_Test()
+        {
+            var mockCalculator = Substitute.For<ICalculator>();
+            mockCalculator.Mode.Returns("DEC");
+            Assert.Equal<string>(mockCalculator.Mode, "DEC");
+
+            mockCalculator.Mode = "HEX";
+            Assert.Equal<string>(mockCalculator.Mode, "HEX"); 
+        }
+
+        [Fact]
+        public void Prac3_Test()
+        {
+            // stick to substituting interfaces
+            var mockCalculator = Substitute.For<ICalculator>();
+
+            mockCalculator.Add(10, -5);
+            mockCalculator.Received().Add(10, Arg.Any<int>());
+            mockCalculator.Received().Add(10, Arg.Is<int>(x => x < 0));
         }
     }
 }
